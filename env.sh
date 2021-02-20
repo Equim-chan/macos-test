23b1c9f37a222e08921f8089044669c5758f8513
Equim <sayaka@ekyu.moe>
2021-02-21 05:57:39 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=846adff45b7d1485.html
-ARGS=(--anonymous -t 2021022014gm-0029-0000-b6a6edeb -a 2 -e -n 0.001 --lang ja)
+OUTPUT=e557a094b4ccdd57.html
+ARGS=(--anonymous -t 2021022105gm-0029-0000-0d882bae -a 3 -e -n 0.001 --lang ja)
