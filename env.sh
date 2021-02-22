40e470e05c5dd874cee3270705ff2e4e80581730
Equim <sayaka@ekyu.moe>
2021-02-22 14:34:08 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-OUTPUT=e557a094b4ccdd57.html
-ARGS=(--anonymous -t 2021022105gm-0029-0000-0d882bae -a 3 -e -n 0.001 --lang ja)
+OUTPUT=170e89551f342e18.html
+ARGS=(--anonymous -m 210222-828d8744-76a4-4cd4-95c2-2e21d86a53dc_a425732306 -e -n 0.001 --lang ja)
